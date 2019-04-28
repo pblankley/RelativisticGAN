362610f1b3b074505e0f6f5b32acfd0b3f4835df
pblankley <31221512+pblankley@users.noreply.github.com>
2019-04-28 11:46:43 -0400

    add ssh scp helpers

---
+++ scp_rel_gan.sh
@@ -0,0 +1,1 @@
+gcloud compute scp paulblankley@rel-gan-inst:~/RelativisticGAN/data/extra_results_cubism/models/* ./results/relgan-cubism-v1/ --zone "us-west2-c"
---
+++ ssh_rel_gan.sh
@@ -0,0 +1,1 @@
+gcloud compute --project "qualai-215800" ssh --zone "us-west2-c" "rel-gan-inst"
